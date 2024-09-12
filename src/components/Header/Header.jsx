4a8346223aecfcd2940e139f871b76e4c849d2b2
AnderWind <iskander.alymovgr@gmail.com>
2024-09-13 00:37:13 +0600

    Finished Header UI and HeroSection UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import styles from './Header.module.css'
+import Button from '../Button/Button';
+import chat from '../../assets/HeaderAssets/chat.svg'
+import user from '../../assets/HeaderAssets/user.svg'
+import userw from '../../assets/HeaderAssets/user-w.svg'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-        <h1>TravelTok</h1>
-        <nav>
-            <a href="/">Home</a>
-            <br />
-        </nav>
+      <h1><a href="/">TRAVELTOK</a></h1>
+      <nav className={styles.header__navbtns}>
+        <a href="#">
+          <Button
+            width='90px'
+            bgColor=''
+            textColor=''
+            text='Чат'
+            icon={chat}
+          ></Button>
+        </a>
+
+        <a href="#">
+          <Button
+            width='160px'
+            bgColor=''
+            textColor=''
+            text='Для авторов'
+            icon={user}
+          ></Button>
+        </a>
+
+        <a href="#">
+          <Button
+            width='110px'
+            bgColor='#FFFFFF'
+            textColor='#000000'
+            text='Войти'
+            icon={userw}
+          ></Button>
+        </a>
+      </nav>
     </header>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import styles from './Home.module.css'
+import searchIcon from '../../assets/HomeAssets/search.svg'
+import dateIcon from '../../assets/HomeAssets/date.svg'
+import regionIcon from '../../assets/HomeAssets/region.svg'
+import priceIcon from '../../assets/HomeAssets/price.svg'
+import FilterButton from '../../components/FilterButton/FilterButton'
 
 const Home = () => {
   return (
@@ -8,7 +13,28 @@ const Home = () => {
         <h1 className={styles.hero_section__title}>ЛУЧШИЙ ПУТЕВОДИТЕЛЬ В КЫРГЫЗСТАНЕ</h1>
         <p className={styles.hero_section__text}>Мы предлагаем уникальные туристические маршруты,<br></br> адаптированные под ваши предпочтения. </p>
         <div className={styles.hero_section__search}>
-          <input type="text" />
+          <div className={styles.hero_section__search__bar}>
+            <div className={styles.hero_section__search__bar__icon}><img src={searchIcon} alt="" /></div>
+            <input className={styles.hero_section__search__bar__input} placeholder='поиск места и мероприятий' type="text" />
+          </div>
+
+          <div className={styles.hero_section__search__filterbtns}>
+            <FilterButton
+            icon={dateIcon}
+            text='дата'
+            isDropdown={true}
+            ></FilterButton>
+            <FilterButton
+            icon={regionIcon}
+            text='область'
+            isDropdown={true}
+            ></FilterButton>
+            <FilterButton
+            icon={priceIcon}
+            text='цена'
+            isDropdown={false}
+            ></FilterButton>
+          </div>
         </div>
       </section>
     </div>
