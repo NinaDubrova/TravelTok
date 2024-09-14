f80182661eff13ad24707b8068cd1815d97cbeac
unknown <alymbechik@gmail.com>
2024-09-14 12:19:44 +0600

    Добавил Области и Футер

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home/Home.jsx'
-import About from '../pages/About/About.jsx'
 import Layout from '../components/Layout/Layout.jsx'
 
 const MainRoutes = () => {
@@ -9,7 +8,6 @@ const MainRoutes = () => {
     <Routes>
         <Route path="/" element={<Layout/>}>
           <Route index element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
         </Route>
     </Routes>
   )
