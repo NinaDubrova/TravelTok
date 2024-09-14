7f952b3119cdb0b3a68b790ce251f84d7d7ad0cb
unknown <alymbechik@gmail.com>
2024-09-14 12:14:12 +0600

    Добавил Области и Футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import styles from './footer.module.css'
+import mail from '../../assets/icons/mail-01.svg'
+import call from '../../assets/icons/call.svg'
+import insta from '../../assets/icons/instagram.svg'
+import threads from '../../assets/icons/threads.svg'
+import x from '../../assets/icons/x.svg'
+import facebook from '../../assets/icons/facebook-02.svg'
+import telegram from '../../assets/icons/telegram.svg'
+
+
 
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; 2024 TravelTok</p>
+            <h1>Traveltok</h1>
+            <nav>
+                <ul>
+                    <li><a href="">О нас</a></li>
+                    <li><a href="">Лицензии</a></li>
+                    <li><a href="">Для авторов</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Маршруты</a></li>
+                    <li><a href="">Q&A</a></li>
+                </ul>
+            </nav>
+            <div className={styles.contact}>
+                <ul>
+                    <li><img src={mail} alt="" /><a href="">traveltok@gmail.com</a></li>
+                    <li><img src={call} alt="" /><a href="">+996 (999) 999-999</a></li>
+                </ul>
+                <div>
+                    <img src={insta} alt="" />
+                    <img src={threads} alt="" />
+                    <img src={x} alt="" />
+                    <img src={facebook} alt="" />
+                    <img src={telegram} alt="" />
+                </div>
+            </div>
         </footer>
     )
 }
---
+++ src/regions/regions.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import RegionCard from '../../components/regionCard/RegionCard';
+import { REGIONS_DATA } from '../../constants/regions';
+import styles from './regions.module.css'
+
+const Regions = () => {
+  return (
+    <div className={styles.regions}>
+        <h1>Области</h1>
+        <div className={styles.container}>
+            {REGIONS_DATA.map((item, idx)=>{
+                return (
+                    <RegionCard
+                    title={item.title}
+                    img={item.icon}
+                />
+                )
+            })}
+            </div>
+    </div>
+  )
+}
+
+export default Regions
