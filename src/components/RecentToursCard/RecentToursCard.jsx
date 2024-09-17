35487d3585260fddf948dd4d9c3733e251636d32
satorwxz <bekakasymbekov23@gmail.com>
2024-09-17 20:49:18 +0600

    Comm

---
+++ src/components/RecentToursCard/RecentToursCard.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from './recentToursCard.module.css'
+import Avatar from '../../assets/icons/Ellipse 2284.svg'
+
+const RecentToursCard = (props) => {
+
+    const {
+        img,
+        star,
+        desc,
+        price,
+        dateStart,
+        dateEnd,
+        starTitle
+    } = props
+
+    return (
+        <div className={styles.card}>
+            <div>
+                <div className={styles.desc}>
+                    <p>{desc}</p>
+                    <img src={Avatar} alt=""/>
+                </div>
+                <img src={img} alt=""/>
+                <div className={styles.info}>
+                    <div className={styles.infoWrapper}>
+                        <h2>{price}</h2>
+                        <div className={styles.star}>
+                            <img src={star} alt=""/>
+                            <h3>{starTitle}</h3>
+                        </div>
+                    </div>
+                    <div className={styles.date}>
+                        <h4>Дата начало:{dateStart}</h4>
+                        <h4>Дата конца:{dateEnd}</h4>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default RecentToursCard
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import styles from './Home.module.css'
 import Regions from '../../modules/regions/regions'
 import { showError, showSuccess } from '../../utils/alerts/alerts'
 import HeroSection from '../../modules/HeroSection/HeroSection'
+import RecentTours from "../../modules/RecentTours/recentTours.jsx";
 
 const Home = () => {
   return (
     <main className={styles.home}>
       <HeroSection/>
       <Regions/>
+        <RecentTours/>
 {/* Toastify */}
       <center>
         <button onClick={() => showSuccess('Toastify-Success')}>Toastify-Success</button>
