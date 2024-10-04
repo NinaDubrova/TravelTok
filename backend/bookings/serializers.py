19394ae794abcf2f2ef8cafd7f109d88af41fd9d
unknown <alymbechik@gmail.com>
2024-10-04 20:11:20 +0600

    Добавил Каталог

--- backend/bookings/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Booking
-
-
-class Booking_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Booking
-        fields = '__all__'
